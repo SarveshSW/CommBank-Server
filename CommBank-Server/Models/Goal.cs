49a28576d4acc94625b46b256ba79f77def69d3c
SarveshSW <sarvesh.wanzare@icloud.com>
2024-07-11 01:24:10 +1000

    Finished backend tasks.

--- CommBank-Server/Models/Goal.cs
+++ CommBank-Server/Models/Goal.cs
@@ -11,6 +11,7 @@ public class Goal
 
     public string? Name { get; set; }
 
+    public string? Icon { get; set; }
     public UInt64 TargetAmount { get; set; } = 0;
 
     public DateTime TargetDate { get; set; }
